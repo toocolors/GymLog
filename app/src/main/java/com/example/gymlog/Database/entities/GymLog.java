9351ffdfac0af4b30785d0144ee0fd0ae295ff3b
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-05 07:10:31 -0700

    Created GymLog.java.

---
+++ app/src/main/java/com/example/gymlog/Database/entities/GymLog.java
@@ -0,0 +1,10 @@
+/**
+ * Title: Gymlog.java
+ * @author Noah deFer
+ * Date: 4/5/2025
+ * Description:
+ */
+package com.example.gymlog.Database.entities;
+
+public class GymLog {
+}
--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -15,9 +15,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.gymlog.databinding.ActivityMainBinding;
-
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
