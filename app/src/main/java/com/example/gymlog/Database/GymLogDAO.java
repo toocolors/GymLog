42494d93564c9f34deb1dfd1000f7ba59015e986
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-05 07:28:56 -0700

    Created GymLogDAO.java.

---
+++ app/src/main/java/com/example/gymlog/Database/GymLogDAO.java
@@ -0,0 +1,13 @@
+/**
+ * Title: GymLogDAO.java
+ * @author Noah deFer
+ * Date: 4/5/2025
+ * Description: DAO for application database.
+ */
+package com.example.gymlog.Database;
+
+import androidx.room.Dao;
+
+@Dao
+public interface GymLogDAO {
+}
--- app/src/main/java/com/example/gymlog/Database/GymLogDatabase.java
+++ app/src/main/java/com/example/gymlog/Database/GymLogDatabase.java
@@ -2,14 +2,16 @@
  * Title: GymLogDatabase.java
  * @author Noah deFer
  * Date: 4/5/2025
- * Description: DAO for GymLog database.
+ * Description: Database object for application database.
  */
 package com.example.gymlog.Database;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.example.gymlog.Database.entities.GymLog;
 
 @Database(entities = {GymLog.class}, version= 1, exportSchema = false)
-public class GymLogDatabase {
+public abstract class GymLogDatabase extends RoomDatabase {
+
 }
