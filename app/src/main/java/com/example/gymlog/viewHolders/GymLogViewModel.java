91d1fd2c2484828a0ea76425579423353c06e0d1
toocolors <188366890+toocolors@users.noreply.github.com>
2025-04-06 11:27:51 -0700

    Created GymLogViewModel.java.

---
+++ app/src/main/java/com/example/gymlog/viewHolders/GymLogViewModel.java
@@ -0,0 +1,12 @@
+/**
+ * Title GymLogViewModel.java
+ * @author Noah deFer
+ * Date: 4/6/2025
+ * Description:
+ */
+package com.example.gymlog.viewHolders;
+
+import androidx.lifecycle.AndroidViewModel;
+
+public class GymLogViewModel extends AndroidViewModel {
+}
